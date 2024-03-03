bb8f09dddcf5c17923a056864ece6ae5fcac5d08
trityazd <161912675+trityazd@users.noreply.github.com>
2024-03-03 20:16:50 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
         <p 
           <code className="font-mono font-bold">2. What do you want to learn or do more of at work? </code> <br></br>
          Learn new technical skills and software tools that is being used widely such as Salesforce, React ; etc.
-        
-            </p>      
+    
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
