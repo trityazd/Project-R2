782a61811a3756edfccad647cb6cc7c7653aa0a2
trityazd <161912675+trityazd@users.noreply.github.com>
2024-03-03 20:29:39 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p 
-          <code className="font-mono font-bold">2. What do you want to learn or do more of at work? </code> <br></br>
-         Learn new technical skills and software tools that is being used widely such as Salesforce, React ; etc.
+        <p >
+          <code className="font-mono font-bold"> 2. What do you want to learn or do more of at work? </code> <br></br>
+         Learn new technical skills and software tools that is being used widely such as Salesforce, React, etc
     
         </p>
+      
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
